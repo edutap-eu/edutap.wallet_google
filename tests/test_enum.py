01775fd56d73394eddc61743c85e38e4096ca06c
Jens W. Klein <jk@kleinundpartner.at>
2023-09-07 20:57:44 +0200

    add simple test for enum

---
+++ tests/test_enum.py
@@ -0,0 +1,21 @@
+def test_camel_case_alias_enum():
+    from edutap.wallet_google.models.primitives.enums import CamelCaseAliasEnum
+
+    class TestFoo(CamelCaseAliasEnum):
+        FOO_BAR_BAZ = "FOO_BAR_BAZ"
+
+    assert TestFoo("fooBarBaz") == TestFoo.FOO_BAR_BAZ
+
+def test_action_enum():
+    from edutap.wallet_google.models.primitives.enums import Action
+
+    assert Action("ACTION_UNSPECIFIED") == Action.ACTION_UNSPECIFIED
+    assert Action("actionUnspecified") == Action.ACTION_UNSPECIFIED
+
+    assert Action("S2AP") == Action.S2AP
+    assert Action("s2ap") == Action.S2AP
+
+    assert Action("SIGN_UP") == Action.SIGN_UP
+    assert Action("signUp") == Action.SIGN_UP
+
+
