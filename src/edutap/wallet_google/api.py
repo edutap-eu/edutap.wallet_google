a0062ed55479b22599493bc3666fc670b4ed98d3
Alexander Loechel <Alexander.Loechel@lmu.de>
2023-09-10 01:34:35 +0200

    implement list method and fix linting errors

--- src/edutap/wallet_google/api.py
+++ src/edutap/wallet_google/api.py
@@ -1,7 +1,5 @@
 from . import registry
 from .models.primitives import Pagination
-from .models.primitives.notification import AddMessageRequest
-from .models.primitives.notification import Message
 from .session import session_manager
 from google.auth.transport.requests import AuthorizedSession
 from pydantic import BaseModel
@@ -40,7 +38,7 @@ def create(
     :return:                   the created model based on the data returned by the API
     """
 
-    session = session_manager.session
+    session: AuthorizedSession = session_manager.session
     data: str
     if model is None and model_data and payload:
         breakpoint()
@@ -77,10 +75,10 @@ def read(
     :raises Exception:         if the response status code is not 200 or 404
     :return:                   the created model based on the data returned by the API
     """
-    session = session_manager.session
+    session: AuthorizedSession = session_manager.session
     if not registry.check_capability(cls=model, capability=registry.Capability.get):
         raise NotImplementedError(
-            f"{capability} not implementend or supported by {model.__name__}"
+            f"{registry.Capability.get} not implementend or supported by {model.__name__}"
         )
     url = _make_url(model, resource_id)
     response = session.get(url=url)
@@ -108,12 +106,27 @@ def update(
     :raises Exception:         if the response status code is not 200 or 404
     :return:                   the created model based on the data returned by the API
     """
-    session = session_manager.session
+    session: AuthorizedSession = session_manager.session
     obj: BaseModel
-    if model_data and model_data.__class__ in registry._MODEL_REGISTRY.keys() and registry.check_capability(cls=model_data.__class__, registry.Capability.update) and not payload:
+    if (
+        model_data
+        and model_data.__class__ in registry._MODEL_REGISTRY.keys()
+        and registry.check_capability(
+            cls=model_data.__class__, capability=registry.Capability.update
+        )
+        and not payload
+    ):
         obj = model_data
         model = model_data.__class__
-    elif model and model in registry._MODEL_REGISTRY.keys() and registry.check_capability(cls=model_data.__class__, registry.Capability.update) and model_data is None and payload:
+    elif (
+        model
+        and model in registry._MODEL_REGISTRY.keys()
+        and registry.check_capability(
+            cls=model_data.__class__, capability=registry.Capability.update
+        )
+        and model_data is None
+        and payload
+    ):
         obj = model(**payload)
     else:
         breakpoint()
@@ -135,6 +148,42 @@ def update(
     return model.model_validate_json(response.content)
 
 
+def patch(
+    model: type,
+    resource_id: str,
+    **payload,
+) -> BaseModel:
+    """
+    Patch-Updates a Google Wallet Class or Object. `U` in CRUD.
+
+    :param model:              model to be patched
+    :param resource_id:        resource id of object / class which should be patched
+    :param payload:            data to pass to the Google RESTful API
+    :raises LookupError:       if the resource was not found (404)
+    :raises Exception:         if the response status code is not 200 or 404
+    :return:                   the created model based on the data returned by the API
+    """
+    session: AuthorizedSession = session_manager.session
+
+    if model in registry._MODEL_REGISTRY.keys() and registry.check_capability(
+        cls=model, capability=registry.Capability.patch
+    ):
+        response = session.patch(
+            url=_make_url(model, resource_id),
+            data=payload.encode("utf-8"),
+        )
+        if response.status_code == 404:
+            raise LookupError(f"{model.__name__} with id: {resource_id} not found")
+
+        if response.status_code != 200:
+            raise Exception(f"Error: {response.status_code} - {response.text}")
+
+        return model.model_validate_json(response.content)
+    raise ValueError(
+        f"{model.__name__} not known or does not support {registry.Capability.patch}"
+    )
+
+
 # def disable(
 # registration_type: RegistrationType,
 # name: str,
@@ -145,6 +194,51 @@ def update(
 #     Generic Implementation of the CRUD --> (D) usually delete, but here disable since delete is not supported at Google Wallets.
 #     """
 #     raise NotImplementedError()
+
+
+def list(
+    model: type,
+    resource_id: str,
+    *,
+    max_results: int = 0,
+    token: str = None,
+) -> list[BaseModel]:
+    session: AuthorizedSession = session_manager.session
+    if model in registry._MODEL_REGISTRY.keys() and registry.check_capability(
+        cls=model, capability=registry.Capability.patch
+    ):
+        params = {}
+        if model.__name__.endswith("Class"):
+            params["issuerId"] = resource_id
+        elif model.__name__.endswith("Object"):
+            params["classId"] = resource_id
+
+        if max_results is not None and max_results > 0:
+            params["maxResults"] = max_results
+        if token is not None:
+            params["token"] = token
+        response = session.get(
+            url=_make_url(model),
+            params=params,
+        )
+        objs = []
+        if response.status_code == 200:
+            data = json.loads(response.content)
+            pagination = Pagination.model_validate(data["pagination"])
+            if pagination.resultsPerPage > 0:
+                raw_objs = data["resources"]
+                for elem in raw_objs:
+                    try:
+                        obj = model.model_validate(elem)
+                        objs.append(obj)
+                    except Exception as e:
+                        # breakpoint()
+                        print(e)
+        elif response.status_code == 404:
+            raise LookupError(f"{model.__name__}: {resource_id} not found")
+        else:
+            raise Exception(f"Error: {response.status_code} - {response.content}")
+        return objs
 
 
 # def list(
