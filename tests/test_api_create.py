0916568651a327fdf00e40088f1819c2edc5893a
Jens W. Klein <jk@kleinundpartner.at>
2023-09-28 14:12:25 +0200

    parametrize method as well

--- tests/test_api_create.py
+++ tests/test_api_create.py
@@ -7,6 +7,7 @@ testdata = [
     (
         "Create",
         "GenericObject",
+        "POST",
         {
             "id": "3388000000022141777.obj53.test.ycc.edutap",
             "classId": "3388000000022141777.test.ycc.edutap",
@@ -16,14 +17,14 @@ testdata = [
 ]
 
 
-@pytest.mark.parametrize("prefix,name,checkdata", testdata)
-def test_api_create(mock_request_response, prefix, name, checkdata):
+@pytest.mark.parametrize("prefix,name,method,checkdata", testdata)
+def test_api_create(mock_request_response, prefix, name, method, checkdata):
     from edutap.wallet_google.api import create
     from edutap.wallet_google.registry import lookup_model
     from edutap.wallet_google.session import session_manager
 
     request_data = mock_request_response(
-        f"{prefix}{name}", session_manager.url(name), "POST"
+        f"{prefix}{name}", session_manager.url(name), method
     )
     result = create(name, request_data["request"]["body"])
 
