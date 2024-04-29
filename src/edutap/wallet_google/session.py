74d977c34295a33539fb7a13c0a222c2da8104b5
Alexander Loechel <Alexander.Loechel@lmu.de>
2024-04-29 20:49:43 +0200

    add pydantic-settings and refactor session to use this.

--- src/edutap/wallet_google/session.py
+++ src/edutap/wallet_google/session.py
@@ -3,6 +3,11 @@ from dotenv import load_dotenv
 from google.auth.transport.requests import AuthorizedSession
 from google.oauth2.service_account import Credentials
 from pathlib import Path
+from pydantic import EmailStr
+from pydantic import Field
+from pydantic import HttpUrl
+from pydantic_settings import BaseSettings
+from pydantic_settings import SettingsConfigDict
 from requests.adapters import HTTPAdapter
 
 import json
@@ -14,9 +19,36 @@ load_dotenv()
 
 _THREADLOCAL = threading.local()
 
+ROOT_DIR = Path(__file__).parent.parent.parent.parent.resolve()
 BASE_URL = "https://walletobjects.googleapis.com/walletobjects/v1"
 SAVE_URL = "https://pay.google.com/gp/v/save"
 SCOPES = ["https://www.googleapis.com/auth/wallet_object.issuer"]
+
+
+class GoogleWalletSettings(BaseSettings):
+    """Settings for Google Wallet Preferences."""
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        env_prefix="EDUTAP_WALLET_GOOGLE_",
+        case_sensitive=False,
+        # extra="ignore",
+        extra="allow",
+    )
+
+    record_api_calls_dir: Path | None = None
+    base_url: HttpUrl = HttpUrl("https://walletobjects.googleapis.com/walletobjects/v1")
+    save_url: HttpUrl = HttpUrl("https://pay.google.com/gp/v/save")
+    scopes: list[HttpUrl] = [
+        HttpUrl("https://www.googleapis.com/auth/wallet_object.issuer")
+    ]
+
+    credentials_path: Path = ROOT_DIR
+
+    credentials_file: str = "credentials.json"
+    issuer_account_email: EmailStr | None = None
+    issuer_id: str | None = Field(min_length=19, max_length=20)
 
 
 class HTTPRecorder(HTTPAdapter):
@@ -49,29 +81,34 @@ class SessionManager:
 
     @property
     def base_url(self) -> str:
-        if getattr(self, "_base_url", None) is None:
-            self._base_url = os.environ.get("EDUTAP_WALLET_GOOGLE_BASE_URL", BASE_URL)
-        return self._base_url
+        return str(self.settings.base_url)
+        # if getattr(self, "_base_url", None) is None:
+        #     self._base_url = os.environ.get("EDUTAP_WALLET_GOOGLE_BASE_URL", BASE_URL)
+        # return self._base_url
 
     @property
     def save_url(self) -> str:
-        if getattr(self, "_save_url", None) is None:
-            self._save_url = os.environ.get("EDUTAP_WALLET_GOOGLE_SAVE_URL", SAVE_URL)
-        return self._save_url
+        return str(self.settings.save_url)
+        # if getattr(self, "_save_url", None) is None:
+        #     self._save_url = os.environ.get("EDUTAP_WALLET_GOOGLE_SAVE_URL", SAVE_URL)
+        # return self._save_url
 
     @property
     def credentials_file(self) -> Path | None:
         if getattr(self, "_credentials_file", None) is None:
             self._credentials_file = None
-            if os.environ.get("EDUTAP_WALLET_GOOGLE_CREDENTIALS_FILE"):
-                path = Path(os.environ.get("CREDENTIAL_PATH", ".")) / Path(
-                    os.environ.get(
-                        "EDUTAP_WALLET_GOOGLE_CREDENTIALS_FILE",
-                        "credential_file.json",
-                    )
-                )
-                if path.exists():
-                    self._credentials_file = path
+            path = self.settings.credentials_path / self.settings.credentials_file
+            if path.exists():
+                self._credentials_file = path
+            # if os.environ.get("EDUTAP_WALLET_GOOGLE_CREDENTIALS_FILE"):
+            #     path = Path(os.environ.get("CREDENTIAL_PATH", ".")) / Path(
+            #         os.environ.get(
+            #             "EDUTAP_WALLET_GOOGLE_CREDENTIALS_FILE",
+            #             "credential_file.json",
+            #         )
+            #     )
+            #     if path.exists():
+            #         self._credentials_file = path
         return self._credentials_file
 
     @property
@@ -87,6 +124,7 @@ class SessionManager:
         return self._credentials_info
 
     def _make_session(self) -> AuthorizedSession:
+        self.settings = GoogleWalletSettings()
         if not self.credentials_file:
             raise ValueError("EDUTAP_WALLET_GOOGLE_CREDENTIALS_FILE not set")
         credentials = Credentials.from_service_account_file(
---
+++ tests/test_settings.py
@@ -0,0 +1,16 @@
+from edutap.wallet_google.session import GoogleWalletSettings
+from pprint import pprint as print
+
+
+def test_settings():
+    settings = GoogleWalletSettings()
+    print(settings.__dict__)
+
+    assert (
+        str(settings.base_url)
+        == "https://walletobjects.googleapis.com/walletobjects/v1"
+    )
+    assert str(settings.save_url) == "https://pay.google.com/gp/v/save"
+    assert settings.scopes == ["https://www.googleapis.com/auth/wallet_object.issuer"]
+    assert settings.issuer_id
+    assert settings.credentials_file
