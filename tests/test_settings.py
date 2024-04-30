950d60b82447756deca169920a36e95ca3b780cf
Alexander Loechel <Alexander.Loechel@lmu.de>
2024-04-30 23:23:31 +0200

    test fixtures

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,6 +1,7 @@
 from edutap.wallet_google.session import GoogleWalletSettings
 from pprint import pprint
 
+import os
 import pytest
 
 
@@ -20,7 +21,9 @@ def test_base_settings():
     ]
 
 
-@pytest.mark.skip("should only be run locally")
+@pytest.mark.skipif(
+    os.environ.get("RUN_ON_CI", False), reason="should only be run locally"
+)
 def test_local_settings():
     settings = GoogleWalletSettings()
     print("Test Settings - Dump Settings Values:")
