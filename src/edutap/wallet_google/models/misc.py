e37e81b8c2fc5475b45f75063bc0c45fc910a7cc
Jens W. Klein <jk@kleinundpartner.at>
2024-12-11 14:59:14 +0100

    merge misc models first level models into one file

--- src/edutap/wallet_google/models/misc.py
+++ src/edutap/wallet_google/models/misc.py
@@ -1,8 +1,94 @@
+"""
+This module contains models that are not directly related to passes but to other Google Wallet APIs
+"""
+
 from ..registry import register_model
 from .bases import Model
+from .bases import WithIdModel
+from .datatypes.general import CallbackOptions
+from .datatypes.message import Message
+from .datatypes.smarttap import IssuerContactInfo
+from .datatypes.smarttap import IssuerToUserInfo
+from .datatypes.smarttap import Permission
+from .datatypes.smarttap import SmartTapMerchantData
 from .passes import generic
 from .passes import retail
 from .passes import tickets_and_transit
+
+
+@register_model(
+    "SmartTap",
+    url_part="smartTap",
+    can_read=False,
+    can_update=False,
+    can_disable=False,
+    can_list=False,
+    can_message=False,
+)
+class SmartTap(WithIdModel):
+    """
+    see: https://developers.google.com/wallet/generic/rest/v1/smarttap#resource:-smarttap
+    """
+
+    # Attribute order as in Google's documentation to make future updates easier!
+    # last check: 2024-11-29
+
+    # inherits id
+    merchantId: str
+    infos: list[IssuerToUserInfo] | None = None
+
+
+@register_model(
+    "Issuer",
+    url_part="issuer",
+    resource_id="issuerId",
+    can_message=False,
+)
+class Issuer(Model):
+    """
+    see: https://developers.google.com/wallet/generic/rest/v1/issuer
+    """
+
+    # Attribute order as in Google's documentation to make future updates easier!
+    # last check: 2024-11-29
+
+    issuerId: str
+    name: str
+    contactInfo: IssuerContactInfo | None = None
+    homepageUrl: str | None = None
+    smartTapMerchantData: SmartTapMerchantData | None = None
+    callbackOptions: CallbackOptions | None = None
+
+
+@register_model(
+    "Permissions",
+    url_part="permissions",
+    resource_id="issuerId",
+    can_disable=False,
+    can_list=False,
+    can_message=False,
+)
+class Permissions(Model):
+    """
+    see: https://developers.google.com/wallet/generic/rest/v1/permissions
+    """
+
+    # Attribute order as in Google's documentation to make future updates easier!
+    # last check: 2024-11-29
+
+    issuerId: str | None = None
+    permissions: list[Permission] = []
+
+
+class AddMessageRequest(Model):
+    """
+    see: https://developers.google.com/wallet/tickets/events/rest/v1/AddMessageRequest
+    """
+
+    # Attribute order as in Google's documentation to make future updates easier!
+    # last check: 2024-11-29
+
+    message: Message | None = None
 
 
 @register_model(
