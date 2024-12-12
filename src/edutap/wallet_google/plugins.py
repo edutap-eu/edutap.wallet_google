a82da936f7008fa864b143c88035a6e989d8e7d9
Robert Niederreiter <rnix@squarewave.at>
2024-12-12 14:26:25 +0100

    Validate plugins in lookup functions

--- src/edutap/wallet_google/plugins.py
+++ src/edutap/wallet_google/plugins.py
@@ -6,9 +6,17 @@ from typing import Iterable
 
 def get_image_providers() -> Iterable[ImageProvider]:
     eps = entry_points(group="edutap.wallet_google.plugins")
-    return [ep.load() for ep in eps if ep.name == "ImageProvider"]
+    plugins = [ep.load() for ep in eps if ep.name == "ImageProvider"]
+    for plugin in plugins:
+        if not isinstance(plugin, ImageProvider):
+            raise ValueError(f"{plugin} not implements ImageProvider")
+    return [plugin() for plugin in plugins]
 
 
 def get_callback_handlers() -> Iterable[CallbackHandler]:
     eps = entry_points(group="edutap.wallet_google.plugins")
-    return [ep.load() for ep in eps if ep.name == "CallbackHandler"]
+    plugins = [ep.load() for ep in eps if ep.name == "CallbackHandler"]
+    for plugin in plugins:
+        if not isinstance(plugin, CallbackHandler):
+            raise ValueError(f"{plugin} not implements CallbackHandler")
+    return [plugin() for plugin in plugins]
