5ee4548db6f1e85a99af62656faba023f8d6adce
Simon Lund <simon97lund@gmail.com>
2025-01-22 22:25:41 +0100

    :recycle: refactor datatypes/datetime.py
    - add 'last check' info at the top
    - add 'DeprecatedKindFieldMixin' to 'TimeInterval'

--- src/edutap/wallet_google/models/datatypes/datetime.py
+++ src/edutap/wallet_google/models/datatypes/datetime.py
@@ -1,6 +1,12 @@
 from ..bases import Model
+from ..deprecated import DeprecatedKindFieldMixin
 
+# TODO: imports itself, i.e. shadows the datetime module
 import datetime
+
+
+# Attribute order as in Google's documentation to make future updates easier!
+# last check: 2025-01-22
 
 
 class DateTime(Model):
@@ -11,7 +17,7 @@ class DateTime(Model):
     date: datetime.datetime
 
 
-class TimeInterval(Model):
+class TimeInterval(DeprecatedKindFieldMixin, Model):
     """
     see: https://developers.google.com/wallet/tickets/events/rest/v1/TimeInterval
     """
