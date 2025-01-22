68d092d236bcc4ef483bb64097a297cd74d99722
Simon Lund <simon97lund@gmail.com>
2025-01-22 23:08:41 +0100

    :recycle: refactor datatypes/message.py
    - add 'last check' info at the top
    - add 'DeprecatedKindFieldMixin' to 'Message' model
    - reorder  attributes of 'Message' to match order in docs

--- src/edutap/wallet_google/models/datatypes/message.py
+++ src/edutap/wallet_google/models/datatypes/message.py
@@ -1,18 +1,23 @@
 from ..bases import Model
+from ..deprecated import DeprecatedKindFieldMixin
 from .datetime import TimeInterval
 from .enums import MessageType
 from .localized_string import LocalizedString
 
 
-class Message(Model):
+# Attribute order as in Google's documentation to make future updates easier!
+# last check: 2025-01-22
+
+
+class Message(DeprecatedKindFieldMixin, Model):
     """
     see: https://developers.google.com/wallet/tickets/events/rest/v1/Message
     """
 
+    header: str | None = None
+    body: str | None = None
+    displayInterval: TimeInterval | None = None
     id: str | None = None
     messageType: MessageType = MessageType.MESSAGE_TYPE_UNSPECIFIED
-    displayInterval: TimeInterval | None = None
-    header: str | None = None
     localizedHeader: LocalizedString | None = None
-    body: str | None = None
     localizedBody: LocalizedString | None = None
