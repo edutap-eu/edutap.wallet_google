007aac93a97504623c50c0d485798ec7b8bc59d5
Simon Lund <simon97lund@gmail.com>
2025-01-23 00:02:31 +0100

    :recycle: refactor datatypes/location.py
    - add 'last check' info at the top
    - add 'DeprecatedKindFieldMixin' to the 'LatLongPoint' model

--- src/edutap/wallet_google/models/datatypes/location.py
+++ src/edutap/wallet_google/models/datatypes/location.py
@@ -1,8 +1,13 @@
 from ..bases import Model
+from ..deprecated import DeprecatedKindFieldMixin
 from pydantic import Field
 
 
-class LatLongPoint(Model):
+# Attribute order as in Google's documentation to make future updates easier!
+# last check: 2025-01-22
+
+
+class LatLongPoint(DeprecatedKindFieldMixin, Model):
     """
     see: https://developers.google.com/wallet/retail/loyalty-cards/rest/v1/LatLongPoint
     """
