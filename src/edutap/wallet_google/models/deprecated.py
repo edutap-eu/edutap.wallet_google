3ea3c9f9d00bee7d288c5e873d54924008737d8e
Simon Lund <simon97lund@gmail.com>
2025-01-23 01:47:27 +0100

    :recycle: refactor bases.py and deprecated.py
    - add todo regarding the default value of 'allowMultipleUsersPerObject'
    - make value of version optional
    - add mixin for 'locations' field
    - move deprecated 'infoModuleData' from bases.py into separate mixin in deprecated.py
    - add mixin for 'wordMark'

--- src/edutap/wallet_google/models/deprecated.py
+++ src/edutap/wallet_google/models/deprecated.py
@@ -1,13 +1,15 @@
-from pydantic import Field
-from typing import Annotated
-from typing_extensions import deprecated
-
-
 """
 Many Google wallet models or datatypes have deprecated attributes.
 Those must not be serialized again, nor repeated in the special models.
 To handle this, we create mixins which can be added to the models which still have the deprecated fields.
 """
+
+from edutap.wallet_google.models.datatypes.data import InfoModuleData
+from edutap.wallet_google.models.datatypes.general import Image
+from edutap.wallet_google.models.datatypes.location import LatLongPoint
+from pydantic import Field
+from typing import Annotated
+from typing_extensions import deprecated
 
 
 class DeprecatedKindFieldMixin:
@@ -41,6 +43,7 @@ class DeprecatedAllowMultipleUsersPerObjectMixin:
                 'Attribute "allowMultipleUsersPerObject" was used in the past to allow multiple users per object but is now deprecated.'
             ),
             exclude=True,
+            # TODO: should this be set to False? or rather None?
             default=False,
         ),
     ]
@@ -53,10 +56,64 @@ class DeprecatedVersionFieldMixin:
     """
 
     version: Annotated[
-        str,
+        str | None,
         Field(
             deprecated=deprecated(
                 'Attribute "version" was used in the past to specify the version of the resource but is now deprecated.'
+            ),
+            exclude=True,
+            default=None,
+        ),
+    ]
+
+
+class DeprecatedLocationsFieldMixin:
+    """
+    Mixin to add the deprecated locations field to a model.
+    May be removed in the future.
+    """
+
+    locations: Annotated[
+        LatLongPoint | None,
+        Field(
+            deprecated=deprecated(
+                'Attribute "locations" was used in the past to specify locations but is now deprecated.'
+            ),
+            exclude=True,
+            default=None,
+        ),
+    ]
+
+
+class DeprecatedInfoModuleDataFieldMixin:
+    """
+    Mixin to add the deprecated infoModuleData field to a model.
+    May be removed in the future.
+    """
+
+    infoModuleData: Annotated[
+        InfoModuleData | None,
+        Field(
+            deprecated=deprecated(
+                'Attribute "infoModuleData" was used in the past to specify info module data but is now deprecated.'
+            ),
+            exclude=True,
+            default=None,
+        ),
+    ]
+
+
+class DeprecatedWordMarkFieldMixin:
+    """
+    Mixin to add the deprecated wordMark field to a model.
+    May be removed in the future.
+    """
+
+    wordMark: Annotated[
+        list[Image] | None,
+        Field(
+            deprecated=deprecated(
+                'Attribute "wordMark" was used in the past to specify the word mark but is now deprecated.'
             ),
             exclude=True,
             default=None,
