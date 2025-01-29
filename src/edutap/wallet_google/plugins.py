00568fcdfb3cd53757275900b042ba47a35d968a
Jens W. Klein <jk@kleinundpartner.at>
2025-01-29 10:44:42 +0100

    Update src/edutap/wallet_google/plugins.py

--- src/edutap/wallet_google/plugins.py
+++ src/edutap/wallet_google/plugins.py
@@ -5,7 +5,7 @@ from importlib.metadata import entry_points
 
 def get_image_providers() -> list[ImageProvider]:
     eps = entry_points(group="edutap.wallet_google.plugins")
-    plugins = [ep.load() for ep in eps if "ImageProvider" in ep.name]
+    plugins = [ep.load() for ep in eps if ep.name.startswith("ImageProvider")]
     if not plugins:
         raise NotImplementedError("No image provider plug-in found")
     for plugin in plugins:
