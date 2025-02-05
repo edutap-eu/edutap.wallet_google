9d1abdc943f1b235488cddb8947ab034212066ef
Jens W. Klein <jk@kleinundpartner.at>
2025-02-05 14:39:34 +0100

    [fix] Allow api usage with dot access from wallet_google namespace

--- src/edutap/wallet_google/__init__.py
+++ src/edutap/wallet_google/__init__.py
@@ -1,2 +1,4 @@
-# import to register models
+# import to enable direkt usage import from parent namespace
+# models also registers the models in the registry
+from . import api  # noqa: F401
 from . import models  # noqa: F401
