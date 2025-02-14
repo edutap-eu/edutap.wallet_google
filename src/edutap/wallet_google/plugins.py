a3bd23c5d1e5d961283c12a18545b804f449541e
Jens W. Klein <jk@kleinundpartner.at>
2025-02-14 11:43:24 +0100

    add plugin at runtime

--- src/edutap/wallet_google/plugins.py
+++ src/edutap/wallet_google/plugins.py
@@ -2,24 +2,31 @@ from .protocols import CallbackHandler
 from .protocols import ImageProvider
 from importlib.metadata import entry_points
 
+_POSSIBLE_PLUGINS = {
+    "ImageProvider": ImageProvider,
+    "CallbackHandler": CallbackHandler,
+}
 
-def get_image_providers() -> list[ImageProvider]:
+_PLUGIN_REGISTRY: dict[str, list[CallbackHandler | ImageProvider]] = {}
+
+def add_plugin(name: str, klass: CallbackHandler | ImageProvider):
+    _PLUGIN_REGISTRY[name].setdefault(name, [])
+    _PLUGIN_REGISTRY[name].append(klass)
+
+def get_plugins(name: str) -> list[CallbackHandler | ImageProvider]:
     eps = entry_points(group="edutap.wallet_google.plugins")
-    plugins = [ep.load() for ep in eps if ep.name.startswith("ImageProvider")]
+    plugins = [ep.load() for ep in eps if ep.name.startswith(name)]
+    plugins += _PLUGIN_REGISTRY.get(name, [])
     if not plugins:
         raise NotImplementedError("No image provider plug-in found")
     for plugin in plugins:
-        if not isinstance(plugin, ImageProvider):
-            raise ValueError(f"{plugin} not implements ImageProvider")
+        if not isinstance(plugin, _POSSIBLE_PLUGINS[name]):
+            raise ValueError(f"{plugin} not implements {name}")
     return [plugin() for plugin in plugins]
+
+def get_image_providers() -> list[ImageProvider]:
+    return get_plugins("ImageProvider")
 
 
 def get_callback_handlers() -> list[CallbackHandler]:
-    eps = entry_points(group="edutap.wallet_google.plugins")
-    plugins = [ep.load() for ep in eps if ep.name.startswith("CallbackHandler")]
-    if not plugins:
-        raise NotImplementedError("No callback handler plugin found.")
-    for plugin in plugins:
-        if not isinstance(plugin, CallbackHandler):
-            raise ValueError(f"{plugin} not implements CallbackHandler")
-    return [plugin() for plugin in plugins]
+    return get_plugins("CallbackHandler")
