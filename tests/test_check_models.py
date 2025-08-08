218a0f9990412b62cacd06196a0ebc5328371af1
Alexander Loechel <alexander.loechel@lmu.de>
2025-08-08 03:03:18 +0200

    detailed test for certain models

--- tests/test_check_models.py
+++ tests/test_check_models.py
@@ -11,28 +11,41 @@ import json
 
 
 def test_check_modles():
-    google_wallet_models = get(
+    discovery_api_data = get(
         "https://discovery.googleapis.com/discovery/v1/apis?name=walletobjects"
     )
-    data = json.loads(google_wallet_models.text)
+    data = json.loads(discovery_api_data.text)
     print("\n")
     print("Google Discorvery API response:")
     print(json.dumps(data, indent=2))
     print("\n")
+    assert len(data["items"]) == 1, "Discovery API response showed more than one API Description"
+    assert data["items"][0]["version"] == "v1", "Discovery API response showed a different version than v1"
+    assert data["items"][0]["id"] == "walletobjects:v1", "Discovery API response showed a different ID than walletobjects:v1"
+    assert data["items"][0]["name"] == "walletobjects"
+    assert data["items"][0]["title"] == "Google Wallet API", "Discovery API response showed a different title than Google Wallet API"
+
     discoveryRestURL = data.get("items", [{}])[0].get("discoveryRestUrl")
-    google_wallet_models = get(discoveryRestURL)
-    data = json.loads(google_wallet_models.text)
+    google_wallet_api_data = get(discoveryRestURL)
+    data = json.loads(google_wallet_api_data.text)
+    assert data["version"] == "v1", "Google Wallet API response showed a different version than v1"
+    assert data["id"] == "walletobjects:v1", "Google Wallet API response showed a different ID than walletobjects:v1"
+    assert data["title"] == "Google Wallet API", "Google Wallet API response showed a different title than Google Wallet API"
+    assert data["protocol"] == "rest", "Google Wallet API response showed a different protocol than rest"
+    print("\n")
+    print(data.keys())
+    # print(json.dumps(data["schemas"], indent=2))
     print("\n")
     print("Google Wallet Models:")
-    print(json.dumps(data, indent=2))
+    # print(json.dumps(data, indent=2))
     print("\n")
     print("Checking models...")
-    for model_name, attributes in data.get("resources", {}).items():
+    for model_name, attributes in data.get("schemas", {}).items():
         print(f"Checking model: {model_name}")
-        if model_name == "issuer":
-            print(json.dumps(Issuer.model_json_schema(), indent=2))
-            print(json.dumps(attributes, indent=2))
-            # breakpoint()
-            continue
+        if model_name == "Issuer":
+            properties_schema = set(Issuer.model_json_schema().get("properties").keys())
+            properties_model = set(attributes.get("properties", {}).keys())
+            assert properties_schema == properties_model, f"Properties mismatch for model {model_name}"
+            print(f"Model {model_name} properties match.")
     data
     print("Finished checking models")
