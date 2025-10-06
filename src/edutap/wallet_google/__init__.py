321c32628e7298022c24f0721daf303d4f25d676
Jens W. Klein <jk@kleinundpartner.at>
2025-10-06 11:43:26 +0200

    Update __init__.py to export new async modules and exceptions
    Export api_async module and exception classes for direct import:
    - api_async: Async API operations
    - WalletException, ObjectAlreadyExistsException, QuotaExceededException
    
    Users can now:
      from edutap.wallet_google import api_async
      from edutap.wallet_google import WalletException

--- src/edutap/wallet_google/__init__.py
+++ src/edutap/wallet_google/__init__.py
@@ -1,5 +1,9 @@
 # import to enable direkt usage import from parent namespace
 # models also registers the models in the registry
 from . import api  # noqa: F401
+from . import api_async  # noqa: F401
 from . import models  # noqa: F401
 from . import utils  # noqa: F401
+from .exceptions import ObjectAlreadyExistsException  # noqa: F401
+from .exceptions import QuotaExceededException  # noqa: F401
+from .exceptions import WalletException  # noqa: F401
