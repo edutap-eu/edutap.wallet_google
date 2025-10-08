34648b6d3b56a33493dd15cd47b306e448bbd1d2
Philipp Auersperg-Castell <phil@bluedynamics.com>
2025-10-08 14:34:22 +0200

    fix integration tests for signature verification

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -102,8 +102,14 @@ def test_add_plugin():
     from edutap.wallet_google.plugins import get_callback_handlers
     from edutap.wallet_google.plugins import get_image_providers
 
-    count_image_providers = len(get_image_providers())
-    count_callback_handlers = len(get_callback_handlers())
+    try:
+        count_image_providers = len(get_image_providers())
+    except NotImplementedError:
+        count_image_providers = 0
+    try:
+        count_callback_handlers = len(get_callback_handlers())  
+    except NotImplementedError:
+        count_callback_handlers = 0 
 
     add_plugin("ImageProvider", DummyImageProvider)
     add_plugin("CallbackHandler", DummyCallbackHandler)
