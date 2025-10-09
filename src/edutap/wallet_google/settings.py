e5e0c8aa19ca36872946d4614c97f83a19dc3444
Jens W. Klein <jk@kleinundpartner.at>
2025-10-09 13:47:23 +0200

    tox -e lint

--- src/edutap/wallet_google/settings.py
+++ src/edutap/wallet_google/settings.py
@@ -38,8 +38,12 @@ class Settings(BaseSettings):
     handler_prefix: str = "/wallet/google"
     handler_prefix_callback: str = ""
     handler_prefix_images: str = ""
-    handler_callback_verify_signature: str = "1"  # "1" enables, "0" disables signature verification
-    handler_callback_verify_expiry: str = "1"  # "1" enables, "0" disables expiration checks (useful for testing with expired data)
+    handler_callback_verify_signature: str = (
+        "1"  # "1" enables, "0" disables signature verification
+    )
+    handler_callback_verify_expiry: str = (
+        "1"  # "1" enables, "0" disables expiration checks (useful for testing with expired data)
+    )
     handler_image_cache_control: str = (
         "no-cache"  # "no-cache", "public, immutable, max-age={max_age}", etc.
     )
